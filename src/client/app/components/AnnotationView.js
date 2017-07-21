7395b38d19f104e17c88f3197e464bd59bca19a6
Jacob Haip <haipjacob@gmail.com>
2017-07-20 23:24:54 -0400

    working on local state for adding annotation

--- src/client/app/components/AnnotationView.js
+++ src/client/app/components/AnnotationView.js
@@ -1,40 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import AnnotationList from "../containers/AnnotationList"
+import AddAnnotationBlock from  "./AddAnnotationBlock"
 
 class AnnotationView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onAddAnnotation = this.onAddAnnotation.bind(this);
-    this.onSaveAnnotation = this.onSaveAnnotation.bind(this);
-    this.onCancelAnnotation = this.onCancelAnnotation.bind(this);
-  }
-
-  onAddAnnotation(e) {
-    e.preventDefault();
-    currentSelectionDetails = markSelection();
-    clearTextSelection();
-    $(".selected-view__new-annotation-input-container").show();
-    $(".selected-view__new-annotation-input").focus();
-  }
-
-  onSaveAnnotation(e) {
-    e.preventDefault();
-    onSaveAnnotation();
-  }
-
-  onCancelAnnotation(e) {
-    e.preventDefault();
-    onCancelAnnotation();
-  }
-
   render() {
     return (
       <div>
-        <input type="submit"
-               value="Add Annotation"
-               className="selected-view__data-add-annotation"
-               onClick={this.onAddAnnotation} />
+        <AddAnnotationBlock />
         <div className="selected-view__new-annotation-input-container">
           <input type="text" className="selected-view__new-annotation-input" />
           <div>
