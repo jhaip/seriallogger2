4edab6ad8917398fc16b419a12bec66217d4ac83
Jacob Haip <haipjacob@gmail.com>
2017-10-19 22:16:04 -0400

    add link to "new" page

--- src/client/app/components/App.js
+++ src/client/app/components/App.js
@@ -22,6 +22,9 @@ class App extends React.Component {
             <div style={{display: "inline-block", paddingRight: "20px"}}>
               <Link to="/notebook/">Notebook</Link>
             </div>
+            <div style={{display: "inline-block", paddingRight: "20px"}}>
+              <Link to="/new/">New</Link>
+            </div>
           </div>
           <div style={{padding: "10px"}}>
             <Route exact path="/" component={DataOverviewPage}/>
