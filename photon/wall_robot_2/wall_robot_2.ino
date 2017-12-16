91b19b73b19dbf99dcd239d41c50259c83289c3f
Jacob Haip <haipjacob@gmail.com>
2017-12-16 16:52:39 -0500

    Photon firmware flash of wall_robot_2

--- photon/wall_robot_2/wall_robot_2.ino
+++ photon/wall_robot_2/wall_robot_2.ino
@@ -1,9 +1,20 @@
+int servoPin = D0;
+Servo myServo;
+int servoPos = 0;
+
 void setup() {
+    myServo.attach(servoPin);
+    myServo.write(67);
     Serial.begin(9600);
 }
 
 void loop() {
-  int val = analogRead(A0);
+  /*int val = analogRead(A0);
   Serial.println(val);
+  delay(1000);*/
+
+  myServo.write(67+10);
+  delay(1000);
+  myServo.write(67-10);
   delay(1000);
 }
