1258cb17bada8abee948db6941d5c2abad7e06c0
Jacob Haip <haipjacob@gmail.com>
2017-12-15 23:03:31 -0500

    update run script to work with the webcam

--- streaming/static/run.sh
+++ streaming/static/run.sh
@@ -2,4 +2,4 @@
 
 watchmedo shell-command --patterns="*.py;*.txt;*.ts" --command='./gen_thumb.sh $watch_event_type $watch_src_path' . &
 
-ffmpeg -f avfoundation -r 30 -i "0" -map 0 -f segment -segment_list playlist.m3u8 -segment_list_flags +live -segment_time 2 -strftime 1 "out%FT%H_%M_%S%z.ts" &
+ffmpeg -f avfoundation -f avfoundation -r 30 -framerate 30.000030 -video_size 640x480 -i "0" -map 0 -f segment -segment_list playlist.m3u8 -segment_list_flags +live -segment_time 2 -strftime 1 "out%FT%H_%M_%S%z.ts" &
