fcdb07972e2194139d527531c288479451a7c9be
Jacob Haip <haipjacob@gmail.com>
2017-12-16 23:38:36 -0500

    updates from 12-16 code testing (mostly notes)

--- streaming/app/app.py
+++ streaming/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from datetime import datetime
 import iso8601
 import pytz
@@ -10,9 +10,11 @@ app = Flask(__name__)
 
 def list_thumbnails(start, end):
     thumbs = os.listdir('../static/thumbs/')
-    print(thumbs)
+    # print(thumbs)
+    app.logger.debug(len(thumbs))
     p = re.compile('out([^\.]*)\.png')
     result = []
+    latest_timestamp = iso8601.parse_date("2000-12-07T23:48:52-0500")
     for t in thumbs:
         #    "out2017-12-07T23_48_52-0500.png"
         # -> "out2017-12-07T23:48:52-0500.png"
@@ -23,11 +25,18 @@ def list_thumbnails(start, end):
         if m:
             t_timestamp = iso8601.parse_date(m.group(1))
             t_timestamp = t_timestamp.astimezone(pytz.utc)
+            latest_timestamp = max(latest_timestamp, t_timestamp)
             if t_timestamp >= start and t_timestamp <= end:
                 result.append({
                     "timestamp": t_timestamp.isoformat(),
                     "url": "http://localhost:8080/play/thumbs/%s" % t
                 })
+        else:
+            app.logger.error("Error parsing {}".format(t))
+            # else:
+            #     app.logger.debug("Value outside range: "+start.isoformat()+" - "+end.isoformat())
+            #     app.logger.debug(t_timestamp.isoformat())
+    app.logger.debug("Latest timestamp: "+latest_timestamp.isoformat()+" ... "+start.isoformat()+" - "+end.isoformat())
     return result
 
 
@@ -39,8 +48,9 @@ def list_thumbnails_view():
         filter_start_date = iso8601.parse_date(filter_start)
         filter_stop_date = iso8601.parse_date(filter_stop)
         thumbnails = list_thumbnails(filter_start_date, filter_stop_date)
+        app.logger.error(len(thumbnails))
         return jsonify({'results': thumbnails})
-    return jsonify({'results': []})
+    return abort(400)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5090, debug=True)
