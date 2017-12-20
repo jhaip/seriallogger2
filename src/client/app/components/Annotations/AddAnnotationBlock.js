5a2109afbecd170c93ea8433791bac3adcfd17fa
Jacob Haip <haipjacob@gmail.com>
2017-12-19 20:36:47 -0500

    give the annotations input space for tweet sized thoughts

--- src/client/app/components/Annotations/AddAnnotationBlock.js
+++ src/client/app/components/Annotations/AddAnnotationBlock.js
@@ -83,8 +83,8 @@ class AddAnnotationBlock extends React.Component {
           this.state.isEditing ?
             <div>
               <h5>New Annotation:</h5>
-              <input type="type"
-                     style={{marginBottom: "5px"}}
+              <textarea
+                     style={{marginBottom: '5px', height: '100px'}}
                      className="selected-view__data-add-annotation-text form-control"
                      onChange={this.handleChange}
                      value={this.state.annotationValue}
