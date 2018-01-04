28442532cc24e6823dfdfe1609ef6b17c46a305c
Jacob Haip <haipjacob@gmail.com>
2018-01-04 18:41:20 -0500

    handle moved files in codemakecode script (for Chrome downloads)

--- makecode/codemakecode.sh
+++ makecode/codemakecode.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-echo "$1 - $2"
-if [ $1 = "created" ]
+echo "$1 - $2 - $3"
+if [ $1 = "created" ] || [ $1 = "moved" ]
 then
-    cp $2 ./test-upload-folder
+    mypath=$2
+    if [ $1 = "moved" ]
+    then
+        mypath=$3
+    fi
+    cp "$mypath" /Volumes/CPLAYBOOT/
     # run node script to convert it to a new .json file
-    node ../uf2-source-code/get_uf2_source_code.js $2
-    git add $2
-    git add "${2/.uf2/.json}"
-    git commit -m "MakeCode code flash of $2"
+    node ../uf2-source-code/get_uf2_source_code.js "$mypath"
+    git add "$mypath"
+    git add "${mypath/.uf2/.json}"
+    git commit -m "MakeCode code flash of $mypath"
 	git push
 fi
--- makecode/runcodemakecode.sh
+++ makecode/runcodemakecode.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-watchmedo shell-command --patterns="*.uf2" --command='./codemakecode.sh $watch_event_type $watch_src_path' .
+watchmedo shell-command --patterns="*.uf2" --command='./codemakecode.sh $watch_event_type "$watch_src_path" "$watch_dest_path"' .
