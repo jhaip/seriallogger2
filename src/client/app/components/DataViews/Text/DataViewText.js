21ee14b1230c88089a80bdca35dd5cff5dce33bf
Jacob Haip <haipjacob@gmail.com>
2018-01-08 23:46:34 -0500

    basic shouldComponentUpdate for DataViewText

--- src/client/app/components/DataViews/Text/DataViewText.js
+++ src/client/app/components/DataViews/Text/DataViewText.js
@@ -3,6 +3,17 @@ import PropTypes from 'prop-types'
 import DataViewTimeGroup from "./DataViewTimeGroup"
 
 class DataViewText extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      this.props.data.length > 0 &&
+      this.props.data.length == nextProps.data.length &&
+      this.props.data[0].id == nextProps.data[0].id &&
+      this.props.data[this.props.data.length-1].id == nextProps.data[nextProps.data.length-1].id
+    ) {
+      return false;
+    }
+    return true;
+  }
   render() {
     const listRows = this.props.data.map((d) =>
       <DataViewTimeGroup key={`${d.sourceName}-${d.id}`}
