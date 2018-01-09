3b4eb8841cf391bb40c62f46e8dc5cdbcb699a94
Jacob Haip <haipjacob@gmail.com>
2018-01-08 23:46:11 -0500

    production webpack config for reference

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,15 @@ var config = {
         use: 'url-loader?name=[name].[ext]'
       }
     ]
-  }
+  },
+  // plugins: [
+  //   new webpack.DefinePlugin({
+  //     'process.env': {
+  //       'NODE_ENV': JSON.stringify('production')
+  //     }
+  //   }),
+  //   new webpack.optimize.UglifyJsPlugin()
+  // ],
 };
 
 module.exports = config;
