9c84e29a98e0774744bab3bb65e036b251c60009
Jacob Haip <haipjacob@gmail.com>
2018-01-09 00:05:57 -0500

    remove logger middleware

--- src/client/app/configureStore.js
+++ src/client/app/configureStore.js
@@ -11,8 +11,7 @@ export default function configureStore(preloadedState) {
     rootReducer,
     composeEnhancers(
       applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
+        thunkMiddleware
       )
     )
   )
